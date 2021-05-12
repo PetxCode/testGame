fa81156e8d2182fa20b1400d37703f140e10dfa8
codelabite <chukwusahpeter@gmail.com>
2021-05-12 21:31:16 +0100

    updating

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const env = require("dotenv");
+const cors = require("cors");
 env.config();
 const DB = "mongodb://localhost:gameChanger";
+const DB_ONLINE =
+  "mongodb+srv://shotkode:shotkode@cluster0.2kfdg.mongodb.net/shotkodeDB?retryWrites=true&w=majority";
+
 const port = process.env.PORT || 4400;
 const app = express();
+const path = require("path");
+const bodyParser = require("body-parser");
+const fileRoutes = require("./routes/file-upload-routes");
 
 mongoose
-  .connect(DB, {
+  .connect(DB_ONLINE, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
@@ -15,9 +22,16 @@ mongoose
     console.log(`Database has been connected successfully...!`);
   });
 
+app.use(cors());
+app.use(bodyParser.json());
+
 app.get("/", async (req, res) => {
   res.send("Hello");
 });
+
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+app.use("/api", fileRoutes.routes);
 
 app.listen(port, () => {
   console.log(`server is listening port: ${port} `);
